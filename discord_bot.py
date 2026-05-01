f887467d94a58457a03cc4e6f8d0b89268179515
ye11oc4t <138095093+ye11oc4t@users.noreply.github.com>
2026-05-01 13:26:07 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -11,7 +11,7 @@ DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 JIRA_BASE_URL = os.getenv("JIRA_BASE_URL")
 JIRA_EMAIL = os.getenv("JIRA_EMAIL")
 JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
-JIRA_PROJECT_KEY = os.getenv("JIRA_PROJECT_KEY", "MADI")
+JIRA_PROJECT_KEY = "MADI"
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
